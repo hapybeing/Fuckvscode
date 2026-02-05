eb6f1fc5e42947a0840faea6c973ceda32703e9f
hapybeing <gaurangk.inbox@gmail.com>
2026-02-05 15:20:26 +0530

    Update StarField.tsx

--- src/components/StarField.tsx
+++ src/components/StarField.tsx
@@ -17,14 +17,20 @@ function MovingStars() {
 
   return (
     <group ref={starsRef}>
-      <Stars radius={50} depth={50} count={5000} factor={4} saturation={0} fade speed={1} />
+      {/* radius: Spread of stars
+        depth: How 'deep' the field goes
+        count: Number of stars (Increased to 8000)
+        factor: Size of stars (Increased to 7 for visibility)
+        saturation: 0 for pure white stars
+      */}
+      <Stars radius={50} depth={50} count={8000} factor={7} saturation={0} fade={false} speed={1} />
     </group>
   );
 }
 
 export default function StarField() {
   return (
-    <div className="fixed top-0 left-0 w-full h-full -z-50 pointer-events-none opacity-40">
+    <div className="fixed top-0 left-0 w-full h-full -z-50 pointer-events-none opacity-60 mix-blend-screen">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <MovingStars />
       </Canvas>
